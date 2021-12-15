32c54f3c62da2c545e0bed0f12f3f7660e21becd
jibujacob <jibupjacob@gmail.com>
2021-12-15 17:11:03 +0530

    Added Swagger Docs

--- Jobs API/server/app.js
+++ Jobs API/server/app.js
@@ -7,6 +7,10 @@ const cors = require("cors")
 const xss = require("xss-clean")
 const rateLimiter = require("express-rate-limit")
 
+//swagger packages
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
 
 const app = express();
 
@@ -33,6 +37,11 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get("/",(req,res)=>{
+  res.send('<h1>Jobs API<h1><a href="/api-docs">Documentation</a>')
+})
+
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 
 //routes
 app.use("/api/v1/auth",authRouter);
